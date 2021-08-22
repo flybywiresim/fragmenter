599e3aa33107071e7c55f334398bc635c5f81830
Niklas Steiner <niklas@sbg.at>
2021-08-22 23:34:22 +0200

    feat: add current install test

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,4 +1,4 @@
-import { pack } from './index';
+import { getCurrentInstall, pack } from './index';
 
 test('Pack', async () => {
     const result = await pack({
@@ -20,4 +20,11 @@ test('Pack', async () => {
     expect(result.base.hash).toBe('c0a7e61ba946b06c58287f913ec159dd22a96ace006d917df4c9dfeb000f9664545ed8211952103051ab5dea82ff87fa4797ec717f117a592c9e3008525c5b59');
     expect(result.base.files).toEqual(['a.json', 'module.json']);
     expect(result.modules).toHaveLength(3);
+});
+
+test('Current Install', () => {
+    const install = getCurrentInstall('./tests/in/update-01');
+
+    expect(install.modules).toHaveLength(10);
+    expect(install.base.hash).toEqual('d5daa0c1a723496883e0f47a2516bf977a96dbb4b03e5506bea1fe821057ff97a0f3730775758b88141169fc18e5e3bc31f6e64fee44237a3890ee27de14f122');
 });
